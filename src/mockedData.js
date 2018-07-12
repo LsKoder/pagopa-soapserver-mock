2c7491bb7947bc5dc03d02afa2b2bca50eeba793
Sergio Manetta <sergiomanetta@yahoo.it>
2018-07-12 11:52:36 +0200

    Update soap response structures

--- src/mockedData.js
+++ src/mockedData.js
@@ -1,71 +1,79 @@
 module.exports = {
   nodoVerificaRPTRispostaOK : {
-    esito: "OK",
-    datiPagamentoPA: {
-      importoSingoloVersamento: 100.52,
-      ibanAccredito: "IT17X0605502100000001234567",
-      bicAccredito: "BPPIITRR",
-      enteBeneficiario: {
-        identificativoUnivocoBeneficiario: {
-          tipoIdentificativoUnivoco: "G",
-          codiceIdentificativoUnivoco: "123"
+    nodoVerificaRPTRisposta: {
+      esito: "OK",
+      datiPagamentoPA: {
+        importoSingoloVersamento: 100.52,
+        ibanAccredito: "IT17X0605502100000001234567",
+        bicAccredito: "BPPIITRR",
+        enteBeneficiario: {
+          identificativoUnivocoBeneficiario: {
+            tipoIdentificativoUnivoco: "G",
+            codiceIdentificativoUnivoco: "123"
+          },
+          denominazioneBeneficiario: "Comune di Canicattì",
+          codiceUnitOperBeneficiario: "01",
+          denomUnitOperBeneficiario: "CDC",
+          indirizzoBeneficiario: "Via Roma",
+          civicoBeneficiario: "23",
+          capBeneficiario: "92010",
+          localitaBeneficiario: "Canicattì",
+          provinciaBeneficiario: "Agrigento",
+          nazioneBeneficiario: "IT"
         },
-        denominazioneBeneficiario: "Comune di Canicattì",
-        codiceUnitOperBeneficiario: "01",
-        denomUnitOperBeneficiario: "CDC",
-        indirizzoBeneficiario: "Via Roma",
-        civicoBeneficiario: "23",
-        capBeneficiario: "92010",
-        localitaBeneficiario: "Canicattì",
-        provinciaBeneficiario: "Agrigento",
-        nazioneBeneficiario: "IT"
-      },
-      credenzialiPagatore: "Mario Rossi",
-      causaleVersamento: "IMU 2018"
+        credenzialiPagatore: "Mario Rossi",
+        causaleVersamento: "IMU 2018"
+      }
     }
   },
 
   nodoVerificaRPTRispostaKO : {
-    fault: {
-      faultCode: "01",
-      faultString: "Fault Error String Example",
-      id: "ID_EXAMPLE",
-    },
-    esito: "KO",
+    nodoVerificaRPTRisposta: {
+      fault: {
+        faultCode: "01",
+        faultString: "Fault Error String Example",
+        id: "ID_EXAMPLE",
+      },
+      esito: "KO"
+    }
   },
 
   nodoAttivaRPTRispostaOK : {
-    esito: "OK",
-    datiPagamentoPA: {
-      importoSingoloVersamento: 100.52,
-      ibanAccredito: "IT17X0605502100000001234567",
-      bicAccredito: "BPPIITRR",
-      enteBeneficiario: {
-        identificativoUnivocoBeneficiario: {
-          tipoIdentificativoUnivoco: "G",
-          codiceIdentificativoUnivoco: "123"
-        },
-        denominazioneBeneficiario: "Comune di Canicattì",
-        codiceUnitOperBeneficiario: "01",
-        denomUnitOperBeneficiario: "CDC",
-        indirizzoBeneficiario: "Via Roma",
-        civicoBeneficiario: "23",
-        capBeneficiario: "92010",
-        localitaBeneficiario: "Canicattì",
-        provinciaBeneficiario: "Agrigento",
-        nazioneBeneficiario: "IT"
-      },    
-      credenzialiPagatore: "Mario Rossi",
-      causaleVersamento: "IMU 2018"
+    nodoAttivaRPTRisposta: {
+      esito: "OK",
+      datiPagamentoPA: {
+        importoSingoloVersamento: 100.52,
+        ibanAccredito: "IT17X0605502100000001234567",
+        bicAccredito: "BPPIITRR",
+        enteBeneficiario: {
+          identificativoUnivocoBeneficiario: {
+            tipoIdentificativoUnivoco: "G",
+            codiceIdentificativoUnivoco: "123"
+          },
+          denominazioneBeneficiario: "Comune di Canicattì",
+          codiceUnitOperBeneficiario: "01",
+          denomUnitOperBeneficiario: "CDC",
+          indirizzoBeneficiario: "Via Roma",
+          civicoBeneficiario: "23",
+          capBeneficiario: "92010",
+          localitaBeneficiario: "Canicattì",
+          provinciaBeneficiario: "Agrigento",
+          nazioneBeneficiario: "IT"
+        },    
+        credenzialiPagatore: "Mario Rossi",
+        causaleVersamento: "IMU 2018"
+      }
     }
   },
 
   nodoAttivaRPTRispostaKO : {
-    fault: {
-      faultCode: "01",
-      faultString: "Fault Error String Example",
-      id: "ID_EXAMPLE",
-    },
-    esito: "KO",
-  },
+    nodoAttivaRPTRisposta : {
+      fault: {
+        faultCode: "01",
+        faultString: "Fault Error String Example",
+        id: "ID_EXAMPLE",
+      },
+      esito: "KO"
+    }
+  }
 }
